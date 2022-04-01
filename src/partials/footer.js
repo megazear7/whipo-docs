b11bb99038bf591fe22c9335aee673cf2e1dcbd1
megazear7 <alexlockahrt7@gmail.com>
2022-04-01 07:54:59 -0400

    Added footer links

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -4,8 +4,8 @@ export default () => html`
   <footer>
     <p>Built with the OrisonJS Starter Kit</p>
     <p><a href="https://www.flaticon.com/free-icons/whisk" title="whisk icons">Whisk icons created by Freepik - Flaticon</a></p>
+    <p><a href="https://www.alexlockhart.me/">Alex Lockhart</a></p>
     <p><a href="https://github.com/megazear7/whipo-docs">GitHub</a></p>
     <p><a href="https://www.npmjs.com/package/whipo">NPM</a></p>
-
   </footer>
 `;
